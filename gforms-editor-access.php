b61df1a21dd0d917ee908eea7fae7d6604a1c444
Randall Rodrigues <randallrodrigues@macbizconsulting.net>
2016-08-26 14:59:02 -0400

    initial commit: simple plugin to add all gform capabilities to the editor user role

---
+++ gforms-editor-access.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Add all Gravity Forms capabilities to Editor role.
+ * Runs during plugin activation.
+ *
+ * @access public
+ * @return void
+ */
+function activate_gform_editor_access() {
+
+  $role = get_role( 'editor' );
+  $role->add_cap( 'gform_full_access' );
+}
+// Register our activation hook
+register_activation_hook( __FILE__, 'activate_gform_editor_access' );
+
+/**
+ * Remove Gravity Forms capabilities from Editor role.
+ * Runs during plugin deactivation.
+ *
+ * @access public
+ * @return void
+ */
+function deactivate_gform_editor_access() {
+
+ $role = get_role( 'editor' );
+ $role->remove_cap( 'gform_full_access' );
+}
+// Register our de-activation hook
+register_deactivation_hook( __FILE__, 'deactivate_gform_editor_access' );
